4ef2b7e8efb57c84ca70802f0daa2f14196b9d8a
Alonso <amuc23@gmail.com>
2025-05-04 18:49:38 -0400

    cambios 2

--- ferremas/settings.py
+++ ferremas/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-60gx)fedy5u0f!3tc7a*4y$xjmv+$4p2@!o)&r)82*3da!7(yl'
 
 # Importante: pon DEBUG en False para producción.
-DEBUG = True  # Se pone True en desarrollo, y antes de subirlo SI O SI EN FALSE (producción)
+DEBUG = False  # Se pone True en desarrollo, y antes de subirlo SI O SI EN FALSE (producción)
 
 # Define los dominios permitidos para producción (en este caso, el dominio de Railway)
 ALLOWED_HOSTS = ['prueba-propia-ferremas-production.up.railway.app', '127.0.0.1', 'localhost']
